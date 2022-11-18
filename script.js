a9517b28625cbbce47cc3982f42b95a75d63be7e
fouad ajayi <fouadajayi6@gmail.com>
2022-11-18 14:09:58 +0100

    New changea

--- script.js
+++ script.js
@@ -40,3 +40,40 @@ btnScrollto.addEventListener('click' , function(){
   section1.scrollIntoView({ behavior: 'smooth' })
 })
 
+document.querySelector('.nav__links').addEventListener('click' , function(e){
+  e.preventDefault()
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    let id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
+
+
+// Tabbbed component
+
+let tabs = document.querySelectorAll('.operations__tab');
+let tabsContainer = document.querySelector('.operations__tab-container');
+let tabsContent = document.querySelectorAll('.operations__content')
+
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
+
